2b10fed2781ac2ee800fec611542254451240aa8
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-07 11:00:49 -0600

    added filler code for cards and home page

---
+++ client/src/components/Cards/warehouseCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+// display
+class warehouseCard extends Component {
+  constructor(props) {
+    super(),
+      (this.state = {
+        id: "",
+      });
+  }
+}
+// async onSubmit
+// try {id}
+// catch
+// render
+
+return (
+  // const {onsubmit} =this
+
+
+
+
+
+
+
+
+
+)
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,15 +1,16 @@
 import React from "react";
-
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 export const Home = () => {
   return (
     <div>
       <h1>Homepage!</h1>
-      <Header> </Header>
+      <Navbar />
+      <Header />
       <h3>in-progress</h3>
-      <Footer> </Footer>
+      <Footer />
     </div>
   );
 };
