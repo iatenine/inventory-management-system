79e6cb17caa6f89dc3f9dd5ccc3b404d7984a4d4
Storm Morzos <storm.morzos@gmail.com>
2021-10-07 12:06:26 -0600

    got semantic/formantic ui working close closes #8

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,19 +1,8 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
 //import { useMutation } from "@apollo/client";
 //import { Link } from "react-router-dom";
 //import { LOGIN } from "../utils/mutations";
 //import Auth from "../utils/auth";
-import {
-  Button,
-  Form,
-  Grid,
-  Header,
-  Image,
-  Message,
-  Segment,
-} from "semantic-ui-react";
-
-//tricking review.OMEGALOL
 
 export const Login = () => {
   // const [formState, setFormState] = useState({ email: "", password: "" });
@@ -41,47 +30,60 @@ export const Login = () => {
   // };
 
   return (
-    <div>
-      <Grid
-        textAlign="center"
-        style={{ height: "100vh" }}
-        verticalAlign="middle"
-      >
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" color="teal" textAlign="center">
-            <Image src="/logo.png" /> Log-in to your account
-          </Header>
-          <Form size="large">
-            {" "}
-            {/*onSubmit={handleFormSubmit}*/}
-            <Segment stacked>
-              <Form.Input
-                fluid
-                icon="user"
-                iconPosition="left"
-                placeholder="E-mail address"
-              />{" "}
-              {/*onChange={handleChange}*/}
-              <Form.Input
-                fluid
-                icon="lock"
-                iconPosition="left"
-                placeholder="Password"
-                type="password"
-              />{" "}
-              {/*onChange={handleChange}*/}
-              <Button color="teal" fluid size="large">
-                Login
-              </Button>
-            </Segment>
-          </Form>
-          <Message>
-            New to us? <a href="#">Sign Up</a>
-          </Message>
-        </Grid.Column>
-      </Grid>
+    <div className="ui center aligned container">
+      <form className="ui form">
+        <div className="field">
+          <label>Username</label>
+          <input type="text" name="Username" placeholder="Username"></input>
+        </div>
+        <div className="field">
+          <label>Password</label>
+          <input type="text" name="Password" placeholder="Password"></input>
+        </div>
+        <button className="ui fluid primary button" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
 
 export default Login;
+
+//  ---OLD INPUT FORM---
+
+//  <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+//    <Grid.Column style={{ maxWidth: 450 }}>
+//      <Header as="h2" color="teal" textAlign="center">
+//        <Image src="https://upload.wikimedia.org/wikipedia/commons/6/69/How_to_use_icon.svg" />{" "}
+//        Log-in to your account
+//      </Header>
+//      <Form size="large">
+//        {" "}
+//        {/*onSubmit={handleFormSubmit}*/}
+//        <Segment stacked>
+//          <Form.Input
+//            fluid
+//            icon="user"
+//            iconPosition="left"
+//            placeholder="E-mail address"
+//          />{" "}
+//          {/*onChange={handleChange}*/}
+//          <Form.Input
+//            fluid
+//            icon="lock"
+//            iconPosition="left"
+//            placeholder="Password"
+//            type="password"
+//          />{" "}
+//          {/*onChange={handleChange}*/}
+//          <Button color="teal" fluid size="large">
+//            Login
+//          </Button>
+//        </Segment>
+//      </Form>
+//      <Message>
+//        New to us? <a href="#">Sign Up</a>
+//      </Message>
+//    </Grid.Column>
+//  </Grid>;
